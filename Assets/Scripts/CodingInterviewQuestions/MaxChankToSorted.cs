bb610347c689605e6fe0f5192f99df602a6c4dd1
MadhusudanGT <madhusudangt97@gmail.com>
2024-12-20 04:40:08 +0530

    Get Non Duplicates Words inside a two given string

--- Assets/Scripts/CodingInterviewQuestions/MaxChankToSorted.cs
+++ Assets/Scripts/CodingInterviewQuestions/MaxChankToSorted.cs
@@ -1,26 +1,60 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using System.Linq;
 
 public class MaxChankToSorted : MonoBehaviour
 {
     [SerializeField] List<int> _listOfNumbers;
+    [Space]
+    [SerializeField] string s1;
+    [SerializeField] string s2;
+    [Header("Not Matched Words from s1 , s2")]
+    [SerializeField] List<string> _listOfWords;
 
     [Button("MaxChankToSorted")]
     public void SortMaxChank()
     {
-        int totalMaxChankCount = 0;
-        int curMaxChank = _listOfNumbers[0];
-        for (int curIndex = 0; curIndex < _listOfNumbers.Count; curIndex++)
+        int runningSum = 0, expectedSum = 0, chunks = 0;
+
+        for (int i = 0; i < _listOfNumbers.Count; i++)
         {
-            curMaxChank = Mathf.Max(curMaxChank, _listOfNumbers[curIndex]);
-            Debug.Log(curMaxChank+"...."+ curIndex+".."+ _listOfNumbers[curIndex]);
-            if (curMaxChank == curIndex)
+            runningSum += _listOfNumbers[i];
+            expectedSum += i;
+            if (runningSum == expectedSum)
             {
-                totalMaxChankCount++;
+                chunks++;
             }
-            Debug.Log(totalMaxChankCount);
+
+            Debug.Log(runningSum + "...." + expectedSum + "....." + chunks);
         }
     }
+
+
+    [Button("MATCHING WORDS IN TWO STRING ")]
+    public void MatchingWords()
+    {//we can make this more better
+        _listOfWords.Clear();
+
+        string[] sentence1 = s1.Split(' ');
+        string[] sentence2 = s2.Split(' ');
+
+        for (int i = 0; i < sentence1.Length; i++)
+        {
+            if (!sentence2.Contains(sentence1[i]) && sentence1.Count(w => w == sentence1[i]) == 1) 
+            {
+                _listOfWords.Add(sentence1[i]);
+            }
+        }
+
+        for (int i = 0; i < sentence2.Length; i++)
+        {
+            if (!sentence1.Contains(sentence2[i]) && sentence2.Count(w => w == sentence2[i]) == 1)
+            {
+                _listOfWords.Add(sentence2[i]);
+            }
+        }
+    }
+
 
 }
