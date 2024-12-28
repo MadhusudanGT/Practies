74c34040dff51af9eb19094afbe56dfa80830e11
MadhusudanGT <madhusudangt97@gmail.com>
2024-12-28 12:16:26 +0530

    solving problems

--- Assets/Scripts/CodingInterviewQuestions/SingleNumber.cs
+++ Assets/Scripts/CodingInterviewQuestions/SingleNumber.cs
@@ -60,4 +60,55 @@ public class SingleNumber : MonoBehaviour
 
         return -1; 
     }
+
+    [Button("PALINDROME")]
+    public void CheckPalindrome()
+    {
+        int left = 0;
+        int right = _listOfNumbers.Count - 1;
+
+        while (left < right)
+        {
+            if (_listOfNumbers[left] == _listOfNumbers[right])
+            {
+                left++;
+                right--;
+            }
+            else
+            {
+                Debug.Log("NOT A PALINDROME");
+                return; 
+            }
+        }
+
+        Debug.Log("GIVEN ARRAY WAS A PALINDROME");
+    }
+    [Button("LONGEST SUB STRING")]
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return 0;
+
+        int maxLength = 0;
+        int start = 0;
+
+        Dictionary<char, int> charIndexMap = new Dictionary<char, int>();
+
+        for (int end = 0; end < s.Length; end++)
+        {
+            char currentChar = s[end];
+
+            if (charIndexMap.ContainsKey(currentChar) && charIndexMap[currentChar] >= start)
+            {
+                start = charIndexMap[currentChar] + 1;
+            }
+
+            charIndexMap[currentChar] = end;
+
+            maxLength = Mathf.Max(maxLength, end - start + 1);
+         //   Debug.Log(start + "...." + end + "...." + currentChar + "..." + charIndexMap[currentChar] + "..." + maxLength);
+        }
+
+        return maxLength;
+    }
 }
