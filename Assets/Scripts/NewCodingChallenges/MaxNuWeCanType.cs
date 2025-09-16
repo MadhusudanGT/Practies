94f4bba922502ef27ae50e9cfd7e4b053efb7a77
MADHU <65445352+MadhusudanGT@users.noreply.github.com>
2025-09-16 08:31:58 +0530

    Longest sub string my version

--- Assets/Scripts/NewCodingChallenges/MaxNuWeCanType.cs
+++ Assets/Scripts/NewCodingChallenges/MaxNuWeCanType.cs
@@ -1,12 +1,15 @@
 using Sirenix.OdinInspector;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 
 public class MaxNuWeCanType : MonoBehaviour
 {
     [SerializeField] private string sentence = string.Empty;
     [SerializeField] private string missingKeys = string.Empty;
+    [SerializeField] private string longestSubString = string.Empty;
 
     [Button("Word Type without Keyboard")]
     public void FindNuOfWordCanType()
@@ -31,5 +34,31 @@ public class MaxNuWeCanType : MonoBehaviour
         Debug.Log("Count..." + count);
     }
 
+    [Button("Longest Sub String !!")]
+    public void LongestSubString()
+    {
+        List<char> strings = new List<char>();
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
 
+        for (int i = 0; i < longestSubString.Length; i++)
+        {
+            if (!strings.Contains(longestSubString[i]))
+            {
+                strings.Add(longestSubString[i]);
+            }
+            else
+            {
+                if (sb.Length < string.Join("", strings).Length)
+                {
+                    sb.Clear();
+                    sb.Append(string.Join("", strings));
+                }
+
+                strings.Clear();
+                strings.Add(longestSubString[i]);
+            }
+        }
+        Debug.Log(string.Join("", strings).Length > sb.Length ? $"Word => {string.Join("", strings)}, Count : {string.Join("", strings).Length}" : $"word : {sb}, count : {sb.Length}");
+    }
 }
